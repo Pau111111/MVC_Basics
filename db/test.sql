dc0bfa63dc89cc46cca8e46db21bcac01cdb7306
Pau111111 <paugarciapellicer96@gmail.com>
2019-12-04 10:42:21 +0100

    database example

---
+++ db/test.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `animals` (
+  `idAnimal` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) DEFAULT NULL,
+  `class` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`idAnimal`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1
+
