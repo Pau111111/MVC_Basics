ebcab304880d912e810da414bf0a31dbae7331aa
Pau111111 <paugarciapellicer96@gmail.com>
2019-12-04 17:10:43 +0100

    xcopy for execute the project on XAMPP

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <!-- https://victorroblesweb.es/2013/11/18/tutorial-mvc-en-php-nativo/ -->
+<!-- XCOPY C:\Users\pau\Documents\Projects\MVC_Basics C:\xampp\htdocs\Projects\MVC_Basics /h/i/c/k/e/r/y -->
 
 <?php
 require_once("db/db.php");
