cb876349dfab86f6c54b46c08747b398d4fa0766
Pau111111 <paugarciapellicer96@gmail.com>
2019-12-04 18:02:43 +0100

    filter by url params

---
+++ README.md
@@ -0,0 +1,3 @@
+## URL_PARAMS
+- action=animals
+
--- controllers/animals_controller.php
+++ controllers/animals_controller.php
@@ -5,5 +5,16 @@ $animals=new animals_model();
 $datos=$animals->get_animals();
  
 //Llamada a la vista
-require_once("views/animals_view.phtml");
+$request_uri = $_SERVER['REQUEST_URI'];
+
+echo $request_uri . "<br/><br/>";
+
+/*$uri_parts = explode('?', $_SERVER['REQUEST_URI'], 2);
+
+echo $uri_parts[1];*/
+
+if ($_GET['action'] == 'animals'){
+    require_once("views/animals_view.phtml");
+}
+
 ?>
